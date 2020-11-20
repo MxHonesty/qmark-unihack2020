473781bea1aa11ba4fbc940d60502546103fd43c
Stelian <stoianstelian33@gmail.com>
2020-11-20 13:29:28 +0200

    fontend init

---
+++ frontend-server/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"; // Import css bootstrap
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pagina React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
